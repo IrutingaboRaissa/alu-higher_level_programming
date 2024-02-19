a180671005ff884bfd848388b7009ab050d3c156
root <r.irutingab@alustudent.com>
2024-02-19 05:21:35 -0800

    100write

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 import sys
-sys.stdarr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+stdarr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
