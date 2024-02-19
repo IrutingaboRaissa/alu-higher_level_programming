9f6d6852a6505f9adae68bd38eb089bcb4b16458
root <r.irutingab@alustudent.com>
2024-02-19 10:07:26 -0800

    alphabets

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for ascii-code in range(ord('a'), ord('z'+1):
-        print("{}".format(chr)(ascii-code)), end='')
-
+for ascii_code in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(ascii_code)), end='')
