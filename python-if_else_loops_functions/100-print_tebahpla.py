59dccf09c502c6b0c0e0d14877f6ebad242c8ed8
root <r.irutingab@alustudent.com>
2024-02-27 09:30:12 -0800

    alphabets in riverse

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    
+for i in range(ord('z'), ord('a') -1, -2):
+     print("{} {}".format(chr(i),chr(i - 33)), end='')
