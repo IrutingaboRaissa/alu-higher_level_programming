cb2bc50b81ca863d1ede1d16454f58b1df65ecc6
IrutingaboRaissa <r.irutingab@alustudent.com>
2024-03-10 10:48:10 -0700

    new rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,6 @@
 class Rectangle:
     """Represent a rectangle"""
 
-
     def __init__(self, width=0, height=0):
         """initialize a new rectangle.
         Args:
@@ -14,10 +13,12 @@ class Rectangle:
             """
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """set the width of the rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,14 +26,16 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """set the height of the rectangle"""
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise ValueError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= o")
+            raise ValueError("height must be >= 0")
         self.__height = value
