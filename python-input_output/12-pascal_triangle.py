732901189c5522cf04fe54941ecd714010f7d939
IrutingaboRaissa <r.irutingab@alustudent.com>
2024-03-12 22:48:23 -0700

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Creates a function pascal_triangle
+'''
+
+
+def pascal_triangle(n):
+    '''Function returns a list of lists of integers
+    representing a pascals triangle
+    '''
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append
