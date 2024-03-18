a662910b7180796e993138b9515d32242f50f21f
IrutingaboRaissa <r.irutingab@alustudent .com>
2024-03-18 10:55:12 +0000

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
--- create a table and add a bunch of values
-CREATE INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)
+-- Cretaes a table and adds a bunch of values
+CREATE TABLE IF NOT EXISTS `second_table` (
+id INT,
+name VARCHAR(256),
+score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
